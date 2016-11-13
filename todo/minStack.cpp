6f835121c26e2d2519c6ef1f431754b8bb543a3c
Chintan Parikh <cparikh@kamama.com>
2016-11-13 06:31:38 -0800

    added folder todo and wip problems

---
+++ todo/minStack.cpp
@@ -0,0 +1,37 @@
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    int getMin() {
+        
+    }
+
+private:
+	stack<int> s_;
+	priority_queue<int, vector<int>, greater<int>> pq_;
+};
+
+int main()
+{
+	return 0;
+}

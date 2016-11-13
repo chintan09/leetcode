8220868811b818a7b350248bab1d1524922c34ca
Chintan Parikh <cparikh@kamama.com>
2016-11-13 07:18:20 -0800

    add isPowerOfThree.cpp

---
+++ isPowerOfThree.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+
+class Solution {
+public:
+	bool isPowerOfThree(int n) {
+		while (n) {
+			if (n % 3 != 0) {
+				return false;
+			}
+			if (n == 3) return true;
+			n = n/3;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution s;
+	printf("%d \n", s.isPowerOfThree(243));
+	printf("%d \n", s.isPowerOfThree(45));
+	return 0;
+}

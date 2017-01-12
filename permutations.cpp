46791448d1235eee917846bafa1e739de364d8f8
Chintan Parikh <cparikh@kamama.com>
2017-01-11 17:01:19 -0800

    add permutation.cpp

---
+++ permutations.cpp
@@ -0,0 +1,26 @@
+#include "lib/misc.h"
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+		vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+		
+		do {
+			result.push_back(nums);
+		} while (next_permutation(nums.begin(), nums.end()));
+        
+		printf("length %d \n", result.size());
+		return result;
+    }
+};
+
+int main()
+{
+	Solution s;
+	
+	vector<int> nums = { 1, 2, 3};
+	s.permute(nums);
+	
+	
+}
